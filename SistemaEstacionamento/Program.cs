5a7c204d5d160e930df8b003ea17b8ca3078e491
Aurus Soares <neiven131@gmail.com>
2024-05-18 22:00:16 -0300

    feat: Add initial implementation  in Program class. Includes the initial settings and displaying a menu

--- SistemaEstacionamento/Program.cs
+++ SistemaEstacionamento/Program.cs
@@ -1,1 +1,41 @@
 ﻿
+using Estacionamento.Common.Models;
+
+// Coloca o encoding para UTF8 para exibir acentuação
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Clear();
+Console.WriteLine("Seja bem-vindo ao sistema!\nPor favor, insira o preço inicial que será cobrado:");
+
+decimal precoInicial;
+string input;
+
+while (true)
+{
+    input = Console.ReadLine();
+    if (decimal.TryParse(input, out precoInicial))
+    {
+        break;
+    }
+    Console.WriteLine("Entrada inválida. Insira um valor válido");
+}
+
+Console.Clear();
+Console.WriteLine("Agora insira o valor que será cobrado por hora:");
+
+decimal precoHora;
+
+while (true)
+{
+    input = Console.ReadLine();
+    if (decimal.TryParse(input, out precoHora))
+    {
+        break;
+    }
+    Console.WriteLine("Entrada inválida. Insira um valor válido");
+}
+
+Estabelecimento estacionamento = new Estabelecimento(precoInicial,precoHora);
+
+estacionamento.ExibirMenu();
+Console.WriteLine("Encerrado");
